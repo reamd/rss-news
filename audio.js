bc7903555932112e8202e2322795c4083dfc5b03
reamdmeng <reamdmeng@tencent.com>
2021-07-26 16:41:21 +0800

    feat: init

---
+++ audio.js
@@ -0,0 +1,51 @@
+import fs from 'fs/promises';
+import ffmpeg from 'fluent-ffmpeg';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+function download(params) {
+  const command = ffmpeg(params.url);
+
+  command
+    .on('start', () => { console.log('Processing started !'); })
+    .on('progress', (progress) => { console.log('Processing: ' + Math.floor(progress.percent) + '% done'); })
+    .on('end', () => {})
+    .on('error', (err) => { console.log('An error occurred: ' + err.message); })
+    .audioCodec('libmp3lame')
+    .audioBitrate('320')
+    .mergeToFile(`./dist/audio/${params.name}.mp3`, './temp_dist');
+}
+
+async function getLastOriUri() {
+  const response = await fetch('https://news.rthk.hk/rthk/ch/pth-news.htm', {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10130'
+    },
+  });
+
+  const $ = cheerio.load(await response.text());
+  const parent = $('#avatar-pos-main-body-top .news');
+  const date = parent.find('.title_date').text().trim();
+  const tName = $(parent.find('.item')[0]).find('.title').text().trim().split(' ')[1];
+  const url = $(parent.find('.item')[0]).find('.listenbuttondiv').find('span').text().trim();
+
+  await fs.writeFile(`./dist/audio/date.json`, JSON.stringify({
+    date,
+  }));
+
+  return {
+    name: tName.replace(':', '_'),
+    url
+  }
+}
+
+async function main () {
+  try {
+    const pObj = await getLastOriUri();
+    download(pObj);
+  } catch (e) {
+    console.log('download audio error', e);
+  }
+}
+
+export default main;
---
+++ index.js
@@ -0,0 +1,65 @@
+import fs from 'fs/promises';
+import AbortController from 'abort-controller';
+import fetch from 'node-fetch';
+import dwAudio from './audio.js';
+
+const controller = new AbortController();
+// 30 秒后取消请求
+const timeout = setTimeout(
+  () => { controller.abort(); },
+  30000,
+);
+
+const urlMap = {
+  HK: 'https://rthk.hk/rthk/news/rss/c_expressnews_clocal.xml',
+  ZH: 'https://rthk.hk/rthk/news/rss/c_expressnews_greaterchina.xml',
+  NT: 'https://rthk.hk/rthk/news/rss/c_expressnews_cinternational.xml',
+  FI: '	https://rthk.hk/rthk/news/rss/c_expressnews_cfinance.xml',
+};
+
+async function initDir() {
+  await fs.rmdir('./dist', { recursive: true });
+  console.log(`successfully deleted ./dist`);
+
+  await fs.mkdir('./dist');
+  console.log(`successfully create ./dist`);
+  
+  await fs.mkdir('./dist/audio');
+  console.log(`successfully create ./dist/audio`);
+}
+
+async function request(obj) {
+  const response = await fetch(obj.url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10130',
+      signal: controller.signal
+    },
+  });
+
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error('wrong status code');
+  }
+
+  await fs.writeFile(`./dist/rss_${obj.key}.xml`, await response.text());
+  console.log(`successfully write rss_${obj.key}.xml`);
+}
+
+async function main() {
+  await initDir();
+  Object.keys(urlMap).forEach(async (item) => {
+    request({
+      key: item,
+      url: urlMap[item]
+    })
+    .catch(err => {
+      console.log(err);
+      process.exit(1);
+    })
+    .finally(() => {
+      clearTimeout(timeout);
+    });
+  });
+  await dwAudio();
+}
+
+main();
